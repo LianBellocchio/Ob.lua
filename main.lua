5fcf9513eee7afff3b1697133a0f2f739b1d9b2d
Lian <103065866+LianBellocchio@users.noreply.github.com>
2023-02-20 20:35:06 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,12 @@
 require("orbwalker")
 require("targetSelector")
+require("utils")
 
 function OnTick()
     local targetSelector = TargetSelector()
     local target = targetSelector:GetTarget()
     Orbwalker:Orbwalk(target)
+    UseSpells()
 end
 
 Callback.Add("Tick", OnTick)
