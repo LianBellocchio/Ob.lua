6e57e528ddf241f30a64c14dea2f094e06f393c8
Lian <103065866+LianBellocchio@users.noreply.github.com>
2023-02-20 20:43:27 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,51 +1,51 @@
 require("orbwalker")
 require("targetSelector")
 require("utils")
-local json = require("json")
 
-local api_key = "RGAPI-0da18bab-778e-4c5f-81d5-a15b56aab8dd"
+local API_KEY = "<tu_api_key_aqui>"
+local REGION = "na1"
+local CHAMPION_DATA_FILE = "champion_data.json"
+local MIN_CHAMPION_GAMES = 100
+
+local function getChampionData()
+    local summonerName = ObjectManager:GetLocalPlayer().Name
+    local url = "https://" .. REGION .. ".api.riotgames.com/lol/summoner/v4/summoners/by-name/" .. summonerName .. "?api_key=" .. API_KEY
+    local response = GetUrl(url)
+    local summonerData = json.decode(response)
+    local summonerId = summonerData.id
+    
+    url = "https://" .. REGION .. ".api.riotgames.com/lol/spectator/v4/active-games/by-summoner/" .. summonerId .. "?api_key=" .. API_KEY
+    response = GetUrl(url)
+    local gameData = json.decode(response)
+    
+    local championData = {}
+    for _, participant in ipairs(gameData.participants) do
+        local championId = participant.championId
+        local championName = GetChampionName(championId)
+        local totalGames = getChampionTotalGames(championId)
+        
+        if totalGames >= MIN_CHAMPION_GAMES then
+            table.insert(championData, {
+                id = championId,
+                name = championName,
+                totalGames = totalGames
+            })
+        end
+    end
+    
+    table.sort(championData, function(a, b) return a.totalGames > b.totalGames end)
+    
+    SaveFile(CHAMPION_DATA_FILE, json.encode(championData))
+end
+
+function OnLoad()
+    getChampionData()
+end
 
 function OnTick()
     local targetSelector = TargetSelector()
     local target = targetSelector:GetTarget()
-
-    local allyChampions = GetAllyChampions()
-    local enemyChampions = GetEnemyChampions()
-
-    local priorities = {}
-    for i, enemy in ipairs(enemyChampions) do
-        local enemyInfo = GetSummonerInfoByName(enemy.name, api_key)
-        local totalDamage = enemyInfo.stats.totalDamageDealtToChampions
-        local totalGold = enemyInfo.stats.goldEarned
-        local priority = totalDamage / totalGold
-        priorities[enemy.name] = priority
-    end
-
-    local sortedPriorities = SortTableByValue(priorities, "desc")
-    local enemyToAttack = nil
-    for i, enemy in ipairs(sortedPriorities) do
-        if CanTarget(target) and target.name == enemy then
-            enemyToAttack = target
-            break
-        end
-
-        for j, champion in ipairs(enemyChampions) do
-            if champion.name == enemy then
-                enemyToAttack = champion
-                break
-            end
-        end
-
-        if enemyToAttack then
-            break
-        end
-    end
-
-    if enemyToAttack then
-        Orbwalker:Orbwalk(enemyToAttack)
-    else
-        Orbwalker:Orbwalk(target)
-    end
+    Orbwalker:Orbwalk(target)
 end
 
 Callback.Add("Tick", OnTick)
