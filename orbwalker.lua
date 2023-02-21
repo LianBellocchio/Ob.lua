f39872440f5d2d74f815e2a3e73c39383cb6c0bd
Lian <103065866+LianBellocchio@users.noreply.github.com>
2023-02-20 21:24:33 -0300

    Update orbwalker.lua

--- orbwalker.lua
+++ orbwalker.lua
@@ -1,24 +1,94 @@
-Orbwalker = {}
-local myHero = myHero
+local orbwalker = {}
 
-function Orbwalker:Orbwalk(target)
-    if CanTarget(target) then
-        if IsMelee(myHero) then
-            if GetDistanceSqr(myHero, target) < GetTrueAttackRange(myHero) * GetTrueAttackRange(myHero) then
-                AttackTarget(target)
-            else
-                MoveToTarget(target)
-            end
-        else
-            if CanAttack() then
-                AttackTarget(target)
-            elseif CanMove() then
-                MoveToTarget(target)
-            end
-        end
-    else
-        if CanMove() then
-            MoveToMouse()
+function orbwalker:AttackTarget(target)
+    if target and GetDistance(myHero, target) <= myHero.range then
+        if CanAttack() then
+            IssueAttack(target)
         end
     end
 end
+
+function orbwalker:MoveToMouse()
+    MoveTo(mousePos.x, mousePos.z)
+end
+
+function orbwalker:CanAttack()
+    return GetTickCount() > lastAttack + attackDelay + 20
+end
+
+function orbwalker:GetMyHeroObject()
+    return myHero
+end
+
+function orbwalker:GetEnemyHeroes()
+    local _enemyHeroes = GetEnemyHeroes()
+    local result = {}
+    for i, enemy in ipairs(_enemyHeroes) do
+        if enemy and enemy.valid and enemy.visible and enemy.dead == false then
+            table.insert(result, enemy)
+        end
+    end
+    return result
+end
+
+function orbwalker:GetTarget()
+    local target = nil
+    local maxPriority = 0
+
+    for i, enemy in ipairs(enemyHeroes) do
+        local priority = GetTargetPriority(enemy)
+
+        if priority > maxPriority and GetDistance(myHero, enemy) <= myHero.range then
+            maxPriority = priority
+            target = enemy
+        end
+    end
+
+    return target
+end
+
+function orbwalker:AttackTarget(target)
+    if target and GetDistance(myHero, target) <= myHero.range then
+        if CanAttack() then
+            IssueAttack(target)
+        end
+    end
+end
+
+function orbwalker:MoveToMouse()
+    MoveTo(mousePos.x, mousePos.z)
+end
+
+function orbwalker:CanAttack()
+    return GetTickCount() > lastAttack + attackDelay + 20
+end
+
+function orbwalker:GetMyHeroObject()
+    return myHero
+end
+
+-- Additional logic to prioritize low health enemies
+function orbwalker:GetLowHealthEnemyTarget()
+    local target = nil
+    local lowestHealth = 1000
+
+    for i, enemy in ipairs(enemyHeroes) do
+        local priority = GetTargetPriority(enemy)
+        local health = enemy.health
+        if priority > 0 and health > 0 and health < lowestHealth and GetDistance(myHero, enemy) <= myHero.range then
+            lowestHealth = health
+            target = enemy
+        end
+    end
+
+    return target
+end
+
+-- Additional logic to prioritize dying enemies
+function orbwalker:GetDyingEnemyTarget()
+    local target = nil
+    local lowestHealthPercent = 100
+
+    for i, enemy in ipairs(enemyHeroes) do
+        local priority = GetTargetPriority(enemy)
+        local health
