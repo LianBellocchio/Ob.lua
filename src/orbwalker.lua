c22482423dd83ea60da2f5d872411e8f6b6cf15e
Lian <103065866+LianBellocchio@users.noreply.github.com>
2023-02-21 04:27:56 -0300

    Update orbwalker.lua

--- src/orbwalker.lua
+++ src/orbwalker.lua
@@ -100,8 +100,81 @@ function orbwalker:GetDyingEnemyTarget()
     end
 
     return target
-<<<<<<< HEAD:src/orbwalker.lua
+function orbwalker:Orbwalk(target)
+    if target and target.isEnemy then
+        if self:CanAttack() then
+            orb.core.attack(target)
+            self.lastAttack = game.time
+        elseif self:CanMove() and self:ShouldMove() then
+            local position = self:GetOrbwalkingPosition(target)
+            if position then
+                orb.core.move(position)
+            end
+        end
+    end
 end
-=======
+
+function orbwalker:ShouldMove()
+    -- Don't move if the target is a structure (e.g. turret, inhibitor)
+    if target and target.isStructure then
+        return false
+    end
+    
+    -- Don't move if the target is in range and we're not trying to last hit
+    if target and orb.core.can_attack() then
+        return false
+    end
+
+    -- Move to the last hit position if we're trying to last hit
+    if self.mode == "LastHit" then
+        local position = self:GetLastHitPosition()
+        if position then
+            return true
+        end
+    end
+
+    -- Move to the orbwalking position for the target
+    local position = self:GetOrbwalkingPosition(target)
+    return position ~= nil
 end
->>>>>>> 6f3ed1b0c101de30f7149145961af1f635e56e51:orbwalker.lua
+
+function orbwalker:GetLastHitPosition()
+    local minionList = self:GetLastHitMinions()
+    if #minionList > 0 then
+        local minion = minionList[1]
+        local predictedDamage = self:GetPredictedDamage(minion)
+        if predictedDamage >= minion.health then
+            return minion.pos
+        end
+    end
+    return nil
+end
+
+function orbwalker:GetLastHitMinions()
+    local minions = objManager.minions
+    local result = {}
+    for i = 0, minions.size[TEAM_ENEMY] - 1 do
+        local minion = minions[TEAM_ENEMY][i]
+        if self:IsValidLastHitMinion(minion) then
+            table.insert(result, minion)
+        end
+    end
+    table.sort(result, function(a, b) return a.health < b.health end)
+    return result
+end
+
+function orbwalker:IsValidLastHitMinion(minion)
+    return minion.isTargetable and minion.isEnemy and not minion.isDead and minion.health > 0 and orb.core.can_attack() and self:GetDistance(minion) <= orbwalker.data.autoAttackRange
+end
+
+function orbwalker:GetPredictedDamage(target)
+    local damage = player.baseAttackDamage + player.flatPhysicalDamageMod
+    local critChance = player.critChance / 100
+    local critDamage = 2
+    if player.critChance == 0 then
+        critDamage = 1
+    end
+    damage = damage * critChance * critDamage + damage * (1 - critChance)
+    return damage
+end
+
